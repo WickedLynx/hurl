23c2a1808305f8907ee7eef994c39aa4dd74a7b8
Harshad Dange <hasse@l1301.core.reaktor.fi>
2018-11-27 16:06:26 +0100

    [app] - Made the file list view scroll properly and added the delete file button

--- hurl-app/src/components/file_list_cell.js
+++ hurl-app/src/components/file_list_cell.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/file_list_cell.css';
 import bytes from 'bytes';
+import TrashIcon from '../assets/trash_icon.png';
 
 export default function FileListCell(props) {
 	var date = new Date(props.file.date_created);
@@ -9,6 +10,9 @@ export default function FileListCell(props) {
 			<p className='font-medium fg-text-dark'>{props.file.name}</p>
 			<p className='font-normal fg-text-dark'>{date.toDateString()}</p>
 			<p className='font-medium fg-text-dark'>{bytes(props.file.size || 0)}</p>
+			<div className='trash-container'>
+			<img src={TrashIcon} />
+			</div>
 		</div>
 	);
 
